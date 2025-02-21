d78c809242deda4bd3e5a749d863edc8eb274e0f
rajat799 <rajatmalapur@gmail.com>
2025-02-21 09:22:57 +0530

    changed class name to downArraow in js file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.querySelector(".downArrow");
+    const downArrow = document.querySelector(".downArrow");
     const navElements = document.querySelector(".nav-elements");
 
-    hamburger.addEventListener("click", function() {
+    downArrow.addEventListener("click", function() {
         navElements.classList.toggle("active");
     });
 });
